155f1722e9d198f84e354bdc341f124241904c12
tingluhk <tingluhk@gmail.com>
2018-04-01 11:10:26 +0800

    chnaged hello.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   root 'welcome#hello'
+  # yo
   # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
