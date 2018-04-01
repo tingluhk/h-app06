f71844c2801e40b7efc92c5bae7ce3bca56970c3
tingluhk <tingluhk@gmail.com>
2018-04-01 23:16:52 +0800

    mobile first fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'generate/Users'
+
+  get 'generate/new'
+
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
