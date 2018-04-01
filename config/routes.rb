7cd1ae98b16ea59c795aaf10e55226564a6257ee
tingluhk <tingluhk@gmail.com>
2018-04-01 11:48:34 +0800

    added controller StaticPages home help

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :microposts
   resources :users
-  root 'welcome#hello'
+  root 'application#hello'
   # yo
   # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
