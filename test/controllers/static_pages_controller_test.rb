9e27a578d325ad5b10c44a0960593e8818a7da4e
tingluhk <tingluhk@gmail.com>
2018-04-01 20:35:54 +0800

    added a new test layout, added new Gem rails-controller-testing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "Tom website Rails"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Tom website Rails"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Tom website Rails"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Tom website Rails"
   end
 
 end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "layout links" do
+    get home_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+  end
+
+
+end
