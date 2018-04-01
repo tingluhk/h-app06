816f3b79bb9a4beb14177ac52c781dea48ba8539
tingluhk <tingluhk@gmail.com>
2018-04-01 12:39:53 +0800

    added shared/header.html.erb changed some views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+  @grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
+  @nums = Array.new(10) { |e| e = e * 2 }
+  @nums1 = Array[1, 2, 3, 4,5]
+
+
+
   def home
+    @bob = 'my name is Bob'
+    @products = [ {'name'=>'Coffee Maker', 'id'=>'1'},{'name'=>'Sugar Brown','id'=>'2'}]
   end
 
   def help
