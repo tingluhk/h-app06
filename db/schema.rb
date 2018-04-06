d4ce831aacadd4f0d69b41fbfab0e0aa569b316e
tingluhk <tingluhk@gmail.com>
2018-04-06 21:15:27 +0800

    added blog scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,7 @@ ActiveRecord::Schema.define(version: 20180405153524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
   create_table "microposts", force: :cascade do |t|
     t.string "content"
